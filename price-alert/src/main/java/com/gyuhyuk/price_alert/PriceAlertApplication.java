2633dfb1818c729016d19e91fa9fa51683aff078
leekh2002 <lkh4342@naver.com>
2025-07-02 16:19:13 +0900

    Initialize micro-promise project

---
+++ price-alert/src/main/java/com/gyuhyuk/price_alert/PriceAlertApplication.java
@@ -0,0 +1,13 @@
+package com.gyuhyuk.price_alert;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PriceAlertApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PriceAlertApplication.class, args);
+	}
+
+}
