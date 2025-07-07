01569b5ae1f990ab81ca03fcc087c3978fc2fa51
leekh2002 <lkh4342@naver.com>
2025-07-07 16:12:59 +0900

    feat: PasswordMatch 어토테이션 추가

--- price-alert/src/main/java/com/gyuhyuk/price_alert/common/annotation/PasswordMatch.java
+++ price-alert/src/main/java/com/gyuhyuk/price_alert/common/annotation/PasswordMatch.java
@@ -1,4 +1,18 @@
 package com.gyuhyuk.price_alert.common.annotation;
 
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@Constraint(validatedBy = PasswordMatchValidator.class)
 public @interface PasswordMatch {
+    String message() default "비밀번호와 비밀번호 확인이 일치하지 않습니다.";
+    Class<?>[] groups() default {};
+    Class<? extends Payload>[] payload() default {};
 }
