7b8ddcb0e0a213a50997b41d61ba81c54654821f
leekh2002 <lkh4342@naver.com>
2026-03-26 19:55:30 +0900

    feat: 프로젝트 참가 엔드포인트 추가

--- micro-promise/src/main/java/com/gyuhyuk/micro_promise/controller/ProjectInviteController.java
+++ micro-promise/src/main/java/com/gyuhyuk/micro_promise/controller/ProjectInviteController.java
@@ -1,10 +1,15 @@
 package com.gyuhyuk.micro_promise.controller;
 
+import com.gyuhyuk.micro_promise.data.dto.AcceptProjectInviteRequest;
 import com.gyuhyuk.micro_promise.data.dto.CustomOAuth2User;
+import com.gyuhyuk.micro_promise.data.dto.ProjectDTO;
+import com.gyuhyuk.micro_promise.data.dto.UserDTO;
 import com.gyuhyuk.micro_promise.service.ProjectInviteService;
+import jakarta.validation.Valid;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,5 +29,18 @@ public class ProjectInviteController {
             @AuthenticationPrincipal CustomOAuth2User user
     ) {
         return projectInviteService.generateInviteCode(projectId, user.getUsername());
+    }
+
+    @PostMapping("/invite/accept")
+    public ProjectDTO acceptInvite(
+            @Valid @RequestBody AcceptProjectInviteRequest request,
+            @AuthenticationPrincipal CustomOAuth2User user
+    ) {
+        UserDTO userDTO = new UserDTO();
+        userDTO.setUsername(user.getUsername());
+        userDTO.setName(user.getName());
+        userDTO.setRole(user.getAuthorities().iterator().next().getAuthority());
+
+        return projectInviteService.acceptProjectInvite(userDTO, request.getCode());
     }
 }
